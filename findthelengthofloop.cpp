d843ecae97ee4f79c343a49ea5c7ae4efb86f22d
Navraj Giri <144989391+Navraj2004@users.noreply.github.com>
2025-04-02 13:55:24 +0530

    Add files via upload

---
+++ findthelengthofloop.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    public:
+      // Function to find the length of a loop in the linked list.
+      int countNodesinLoop(Node *head) {
+          // Code here
+          
+          
+          Node *slow=head;
+          Node *fast=head;
+          
+          
+          while(fast!=NULL && fast->next!=NULL)
+          {
+              slow=slow->next;
+              fast=fast->next->next;
+              
+              if(slow==fast)
+              {
+                  break;
+              }
+          }
+          
+          
+          
+          
+          
+          if(fast==NULL || fast->next==NULL)
+          {
+              return 0;
+          }
+          
+          int count=1;
+          slow=fast->next;
+          
+          while(slow!=next)
+          {
+              count++;
+              slow=slow->next;
+          }
+          
+          
+          return count;
+          
+      }
+  };
+  
