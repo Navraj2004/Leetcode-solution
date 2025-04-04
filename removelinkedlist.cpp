a9a02935aaa7379a6e66c62c600689ed4a93ced0
Navraj Giri <144989391+Navraj2004@users.noreply.github.com>
2025-04-04 12:47:02 +0530

    Add files via upload

---
+++ removelinkedlist.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    public:
+        ListNode* removeElements(ListNode* head, int val) {
+            
+    
+          if(head==NULL)
+          {
+            return head;
+          }
+          
+         while(head!=NULL && head->val==val)
+         {
+             ListNode* temp = head;
+             head=head->next;
+             delete temp;
+         }
+    
+    
+    
+        ListNode *curr=head;
+        ListNode *prev=NULL;
+         while(curr!=NULL)
+         {
+           
+            if(curr->val==val)
+            {
+                prev->next=curr->next;
+                delete curr;
+                curr=prev->next;
+            }
+    
+          else{
+            prev=curr;
+            curr=curr->next;
+          }
+         }
+    
+          return head;
+        }
+    };
